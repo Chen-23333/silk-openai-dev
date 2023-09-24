e3f069e23ad5f66e62e29ad2681235f707846d9c
辰 <479577719@qq.com>
2023-09-24 15:59:11 +0800

    更新配置文件读取方式

--- openai-api/example_curl.py
+++ openai-api/example_curl.py
@@ -2,11 +2,19 @@
 
 import json
 import requests
+import os
+import subprocess
+
+# 通过 subprocess 执行 shell 命令，获取 git 仓库的根目录
+command = ['git', 'rev-parse', '--show-toplevel']
+process = subprocess.Popen(command, stdout=subprocess.PIPE)
+output, error = process.communicate()
+git_root = output.decode().strip()
 
 # 通过 curl 请求 openai 示例
-
+config_path = os.path.join(git_root, "config.json")
 config = {}
-with open("config.json","r") as f:
+with open(config_path,"r") as f:
     config = json.load(f)
 
 sk = config["sk"]
--- openai-api/example_python.py
+++ openai-api/example_python.py
@@ -2,16 +2,24 @@
 
 import json
 import openai
+import os
+import subprocess
 
-# 通过 python 库请求 openai 示例
+# 通过 subprocess 执行 shell 命令，获取 git 仓库的根目录
+command = ['git', 'rev-parse', '--show-toplevel']
+process = subprocess.Popen(command, stdout=subprocess.PIPE)
+output, error = process.communicate()
+git_root = output.decode().strip()
 
+config_path = os.path.join(git_root, "config.json")
 config = {}
-with open("config.json","r") as f:
+with open(config_path,"r") as f:
     config = json.load(f)
 
 openai.api_key = config["sk"]
 message = config["content"]
 
+# 通过 python 库请求 openai 示例
 completion = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
   messages=[
