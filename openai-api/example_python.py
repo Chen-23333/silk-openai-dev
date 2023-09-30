85554880c95ad02853dd9baa9d7f508e9562c944
辰 <479577719@qq.com>
2023-09-30 23:28:34 +0800

    添加函数调用演示

--- openai-api/example_python.py
+++ openai-api/example_python.py
@@ -21,11 +21,11 @@ message = config["content"]
 
 # 通过 python 库请求 openai 示例
 completion = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": message}
-  ]
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": message}
+    ]
 )
 
 print('\n')
